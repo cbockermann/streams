631e8a551281d9d4e3bdaa2e39b67ff7660ba892
h_blom <hendrik.blom@udo.edu>
2012-04-02 10:31:50 +0200

    new Feature: SelectAttributes without removal of the other Attributes from original dataItem. Return a copy of the selected Attributes.

--- stream-api/src/main/java/stream/data/mapper/SelectAttributes.java
+++ stream-api/src/main/java/stream/data/mapper/SelectAttributes.java
@@ -9,6 +9,8 @@ import java.util.Set;
 
 import stream.data.AbstractDataProcessor;
 import stream.data.Data;
+import stream.data.DataImpl;
+import stream.data.DataProcessor;
 import stream.util.ParameterUtils;
 
 /**
@@ -20,6 +22,12 @@ public class SelectAttributes extends AbstractDataProcessor {
 	String[] keys = new String[0];
 
 	Set<String> selected = new HashSet<String>();
+	private Boolean remove;
+
+	public SelectAttributes() {
+		super();
+		this.remove = true;
+	}
 
 	public void setKeys(String keyString) {
 		keys = ParameterUtils.split(keyString);
@@ -27,8 +35,22 @@ public class SelectAttributes extends AbstractDataProcessor {
 			selected.add(key);
 	}
 
+	public DataProcessor setKeys(Set<String> keys) {
+		this.keys = keys.toArray(new String[keys.size()]);
+		selected = keys;
+		return this;
+	}
+
 	public String getKeys() {
 		return ParameterUtils.join(keys);
+	}
+
+	public void setRemove(Boolean remove) {
+		this.remove = remove;
+	}
+
+	public Boolean getRemove() {
+		return this.remove;
 	}
 
 	/**
@@ -36,19 +58,27 @@ public class SelectAttributes extends AbstractDataProcessor {
 	 */
 	@Override
 	public Data process(Data data) {
-
-		if (keys == null || keys.length == 0) {
+		if (keys == null || keys.length == 0)
 			return data;
-		}
 
 		Iterator<String> it = data.keySet().iterator();
-		while (it.hasNext()) {
-			String key = it.next();
-			if (!selected.contains(key)) {
-				it.remove();
+		if (remove) {
+			while (it.hasNext()) {
+				String key = it.next();
+				if (!selected.contains(key)) {
+					it.remove();
+				}
 			}
+			return data;
+		} else {
+			Data result = new DataImpl();
+			while (it.hasNext()) {
+				String key = it.next();
+				if (selected.contains(key)) {
+					result.put(key, data.get(key));
+				}
+			}
+			return result;
 		}
-
-		return data;
 	}
 }

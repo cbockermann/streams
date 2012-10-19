5e1297556c432a374026bdea039415c233f6c9b5
hblom <hendrik.blom@udo.edu>
2012-10-19 16:38:07 +0200

    Added initialized and finished flag to SimpleMockProcessor.

--- stream-api/src/test/java/stream/mock/SimpleMockProcessor.java
+++ stream-api/src/test/java/stream/mock/SimpleMockProcessor.java
@@ -1,14 +1,25 @@
 package stream.mock;
 
 import stream.AbstractProcessor;
+import stream.ProcessContext;
 import stream.data.Data;
 
 public class SimpleMockProcessor extends AbstractProcessor {
 
+	private Boolean initialized;
 	private Boolean processed;
+	private Boolean finished;
 
 	public SimpleMockProcessor() {
 		processed = false;
+	}
+
+	public Boolean getInitialized() {
+		return initialized;
+	}
+
+	public void setInitialized(Boolean initialized) {
+		this.initialized = initialized;
 	}
 
 	public Boolean getProcessed() {
@@ -19,10 +30,30 @@ public class SimpleMockProcessor extends AbstractProcessor {
 		this.processed = processed;
 	}
 
+	public Boolean getFinished() {
+		return finished;
+	}
+
+	public void setFinished(Boolean finished) {
+		this.finished = finished;
+	}
+
+	@Override
+	public void init(ProcessContext ctx) throws Exception {
+		super.init(ctx);
+		initialized = true;
+	}
+
 	@Override
 	public Data process(Data input) {
 		processed = true;
 		return input;
+	}
+
+	@Override
+	public void finish() throws Exception {
+		super.finish();
+		finished = true;
 	}
 
 }

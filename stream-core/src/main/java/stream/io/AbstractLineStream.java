34061b87d952876fa67ec3c7cd111e45ff4fdbe9
Hendrik Blom <hendrik.blom@udo.edu>
2013-11-18 09:28:34 +0100

    Added encoding

--- stream-core/src/main/java/stream/io/AbstractLineStream.java
+++ stream-core/src/main/java/stream/io/AbstractLineStream.java
@@ -10,13 +10,22 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * @author chris
+ * @author chris,hendrik
  * 
  */
 public abstract class AbstractLineStream extends AbstractStream {
 
 	protected Map<String, Class<?>> attributes;
 	protected BufferedReader reader;
+	protected String encoding;
+
+	public String getEncoding() {
+		return encoding;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
 
 	/**
 	 * @param in
@@ -35,6 +44,7 @@ public abstract class AbstractLineStream extends AbstractStream {
 	public AbstractLineStream() throws Exception {
 		super();
 	}
+
 	/**
 	 * @see stream.io.Source#close()
 	 */
@@ -51,7 +61,12 @@ public abstract class AbstractLineStream extends AbstractStream {
 	public void init() throws Exception {
 		super.init();
 		attributes = new LinkedHashMap<String, Class<?>>();
-		reader = new BufferedReader(new InputStreamReader(getInputStream()));
+
+		if (encoding != null)
+			reader = new BufferedReader(new InputStreamReader(getInputStream(),
+					encoding));
+		else
+			reader = new BufferedReader(new InputStreamReader(getInputStream()));
 	}
 
 	public String readLine() throws Exception {

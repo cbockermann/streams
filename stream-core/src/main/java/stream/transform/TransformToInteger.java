fba756cc77055229314d5ee6e575ad1aa362df7a
Hendrik Blom <hendrik.blom@udo.edu>
2014-03-07 11:49:52 +0100

    Added comments

--- stream-core/src/main/java/stream/transform/TransformToInteger.java
+++ stream-core/src/main/java/stream/transform/TransformToInteger.java
@@ -5,16 +5,24 @@ import java.io.Serializable;
 import stream.AbstractProcessor;
 import stream.Data;
 
+/**
+ * 
+ * Transform the values of the given keys to Integer, if the values are a
+ * instance of Number.
+ * 
+ * @author Hendrik Blom
+ * 
+ */
 public class TransformToInteger extends AbstractProcessor {
 
 	private String[] keys;
-	
+
 	@Override
 	public Data process(Data data) {
-		for(String key: keys){
+		for (String key : keys) {
 			Serializable s = data.get(key);
-			if(s!=null && s instanceof Number)
-				data.put(key, ((Number)s).intValue());
+			if (s != null && s instanceof Number)
+				data.put(key, ((Number) s).intValue());
 		}
 		return data;
 	}

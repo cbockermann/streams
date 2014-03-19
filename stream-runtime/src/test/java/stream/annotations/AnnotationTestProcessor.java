73141d6269bc746c403df7b62ff6b0747b2037b1
Christian Bockermann <chris@jwall.org>
2014-03-19 16:48:44 +0100

    Fixed test - @Parameter annotated parameters are optional by default

--- stream-runtime/src/test/java/stream/annotations/AnnotationTestProcessor.java
+++ stream-runtime/src/test/java/stream/annotations/AnnotationTestProcessor.java
@@ -4,57 +4,52 @@ import stream.Data;
 import stream.Processor;
 
 /**
- * Test processor for xmlAnnotations
- * Created by bruegge on 3/5/14.
+ * Test processor for xmlAnnotations Created by bruegge on 3/5/14.
  */
 
 public class AnnotationTestProcessor implements Processor {
 
-    @Parameter(name = "differentXMLName")
-    private String namedParameter;
+	@Parameter(name = "differentXMLName")
+	private String namedParameter;
 
-    @Parameter
-    private String nonoptional;
+	@Parameter(required = true)
+	private String nonoptional;
 
-    @Parameter(required=false)
-    private String optional;
+	@Parameter
+	private String optional;
 
+	@Override
+	public Data process(Data input) {
+		System.out.println("Annotation test processor:  ");
+		System.out.println("nonoptional: " + nonoptional);
+		System.out.println("optional: " + optional);
+		return input;
+	}
 
-    @Override
-    public Data process(Data input) {
-        System.out.println("Annotation test processor:  ");
-        System.out.println("nonoptional: " + nonoptional);
-        System.out.println("optional: " + optional);
-        return input;
-     }
+	// getter and setter crap for our fields
 
+	public String getNonoptional() {
+		return nonoptional;
+	}
 
-    //getter and setter crap for our fields
+	public void setNonoptional(String nonoptional) {
+		this.nonoptional = nonoptional;
+	}
 
-    public String getNonoptional() {
-        return nonoptional;
-    }
+	public String getOptional() {
+		return optional;
+	}
 
-    public void setNonoptional(String nonoptional) {
-        this.nonoptional = nonoptional;
-    }
+	public void setOptional(String optional) {
+		this.optional = optional;
+	}
 
-    public String getOptional() {
-        return optional;
-    }
+	public String getNamedParameter() {
+		return namedParameter;
+	}
 
-    public void setOptional(String optional) {
-        this.optional = optional;
-    }
-
-
-    public String getNamedParameter() {
-        return namedParameter;
-    }
-
-    public void setNamedParameter(String namedParameter) {
-        this.namedParameter = namedParameter;
-    }
-
+	public void setNamedParameter(String namedParameter) {
+		this.namedParameter = namedParameter;
+	}
 
 }

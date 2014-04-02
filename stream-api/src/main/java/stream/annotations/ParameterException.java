88fc010871166af301118a0a2093b0fa3ffa9c8c
Christian Bockermann <chris@jwall.org>
2014-04-02 11:26:49 +0200

    Added missing "serialVersionUID"

--- stream-api/src/main/java/stream/annotations/ParameterException.java
+++ stream-api/src/main/java/stream/annotations/ParameterException.java
@@ -3,29 +3,30 @@ package stream.annotations;
 import java.lang.reflect.Field;
 
 /**
- * Exception that should occur when a nonoptional field is not set in the xml file.
- * Or a field annotated as xmlparameter doesnt have a setter method
+ * Exception that should occur when a nonoptional field is not set in the xml
+ * file. Or a field annotated as parameter doesnt have a setter method.
+ * 
  * Created by bruegge on 3/5/14.
  */
 public class ParameterException extends RuntimeException {
 
-    private Field missingField;
+	/** The unique class ID */
+	private static final long serialVersionUID = -174510552581686807L;
+	private Field missingField;
 
-    public ParameterException(String message){
-        super(message);
-    }
+	public ParameterException(String message) {
+		super(message);
+	}
 
-    public ParameterException(String message, Field missingField){
-        super(message);
-    }
+	public ParameterException(String message, Field missingField) {
+		super(message);
+	}
 
+	public Field getMissingField() {
+		return missingField;
+	}
 
-
-    public Field getMissingField() {
-        return missingField;
-    }
-
-    public void setMissingField(Field missingField) {
-        this.missingField = missingField;
-    }
+	public void setMissingField(Field missingField) {
+		this.missingField = missingField;
+	}
 }

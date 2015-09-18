1eaebdf00374125a430001db254dbc1049150d96
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2015-09-18 11:34:10 +0100

    Add README and remove unused File. Also add another test

---
+++ README.md
@@ -0,0 +1,7 @@
+# The streams framework
+
+A data streaming framework that uses xml files for process design. For more information visit
+
+[https://sfb876.de/streams/](https://sfb876.de/streams/)
+
+
---
+++ stream-core/src/test/java/stream/flow/QueueCopyTest.java
@@ -0,0 +1,54 @@
+/*
+ *  streams library
+ *
+ *  Copyright (C) 2011-2014 by Christian Bockermann, Hendrik Blom
+ * 
+ *  streams is a library, API and runtime environment for processing high
+ *  volume data streams. It is composed of three submodules "stream-api",
+ *  "stream-core" and "stream-runtime".
+ *
+ *  The streams library (and its submodules) is free software: you can 
+ *  redistribute it and/or modify it under the terms of the 
+ *  GNU Affero General Public License as published by the Free Software 
+ *  Foundation, either version 3 of the License, or (at your option) any 
+ *  later version.
+ *
+ *  The stream.ai library (and its submodules) is distributed in the hope
+ *  that it will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+ *  warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+package stream.flow;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Test to push data from from several processes into one queue in parallel
+ * @author Kai
+ * 
+ */
+public class QueueCopyTest {
+
+	static Logger log = LoggerFactory.getLogger(QueueCopyTest.class);
+
+	@Test
+	public void test() {
+		try {
+			URL url = QueueCopyTest.class.getResource("/test-queue-copies.xml");
+			stream.run.main(url);
+
+		} catch (Exception e) {
+			log.error("Test failed: {}", e.getMessage());
+			fail("Test failed: " + e.getMessage());
+		}
+	}
+}

108b138d9d52a34b646d48e3ea42b472a21ad5fe
Christian Bockermann <chris@jwall.org>
2016-01-13 12:10:54 +0100

    The streams-storm adaption is now a separate project.

--- stream-storm/src/main/java/stream/storm/Constants.java
+++
@@ -1,9 +0,0 @@
-package stream.storm;
-
-/**
- * Created by alexey on 19/08/15.
- */
-public class Constants {
-    public static final String ID = "id";
-    public static final String NUM_WORKERS = "stream.storm.workers";
-}

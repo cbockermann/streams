392cc2e2b7101028203194bc94e643f3790dab84
Kai Bruegge <kai.bruegge@udo.edu>
2016-01-14 16:04:55 +0100

    add comment to testservice

--- streams-runtime/src/test/java/streams/runtime/TestService.java
+++ streams-runtime/src/test/java/streams/runtime/TestService.java
@@ -3,6 +3,7 @@ package streams.runtime;
 import stream.service.Service;
 
 /**
+ * Test service for serviceinjection tests. There is not a single simple Service implementation in streams.
  * Created by kai on 14.01.16.
  */
 public class TestService implements Service {

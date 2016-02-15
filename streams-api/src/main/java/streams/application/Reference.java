b7d17f26967abc42ae58ef73ff88569594ede9b3
Christian Bockermann <chris@jwall.org>
2016-02-15 17:45:13 +0100

    Fixed wrong package name.

--- streams-api/src/main/java/streams/application/Reference.java
+++ streams-api/src/main/java/streams/application/Reference.java
@@ -21,7 +21,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see http://www.gnu.org/licenses/.
  */
-package stream.app;
+package streams.application;
 
 /**
  * <p>

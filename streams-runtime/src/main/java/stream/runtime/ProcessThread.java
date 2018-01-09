ac9a618fc33f0f743262f7f2f66300550839d3ae
Christian Bockermann <chris@jwall.org>
2018-01-09 08:09:51 +0100

    Added print-out of error stacktrace in case of process failure.

--- streams-runtime/src/main/java/stream/runtime/ProcessThread.java
+++ streams-runtime/src/main/java/stream/runtime/ProcessThread.java
@@ -46,141 +46,142 @@ import streams.runtime.Hook;
  */
 public class ProcessThread extends Thread implements Hook {
 
-    static Logger log = LoggerFactory.getLogger(ProcessThread.class);
+	static Logger log = LoggerFactory.getLogger(ProcessThread.class);
 
-    static final Map<String, Integer> PRIORITY_NAMES = new LinkedHashMap<String, Integer>();
+	static final Map<String, Integer> PRIORITY_NAMES = new LinkedHashMap<String, Integer>();
 
-    static {
-        PRIORITY_NAMES.put("lowest", Thread.MIN_PRIORITY);
-        PRIORITY_NAMES.put("low", 2);
-        PRIORITY_NAMES.put("normal", Thread.NORM_PRIORITY);
-        PRIORITY_NAMES.put("high", 7);
-        PRIORITY_NAMES.put("highest", Thread.MAX_PRIORITY);
-    }
+	static {
+		PRIORITY_NAMES.put("lowest", Thread.MIN_PRIORITY);
+		PRIORITY_NAMES.put("low", 2);
+		PRIORITY_NAMES.put("normal", Thread.NORM_PRIORITY);
+		PRIORITY_NAMES.put("high", 7);
+		PRIORITY_NAMES.put("highest", Thread.MAX_PRIORITY);
+	}
 
-    final stream.Process process;
-    final ApplicationContext context;
+	final stream.Process process;
+	final ApplicationContext context;
 
-    final AtomicBoolean executing = new AtomicBoolean(false);
-    boolean running = false;
+	final AtomicBoolean executing = new AtomicBoolean(false);
+	boolean running = false;
 
-    protected final List<ProcessListener> processListener = new ArrayList<ProcessListener>();
+	protected final List<ProcessListener> processListener = new ArrayList<ProcessListener>();
 
-    public ProcessThread(stream.Process process, ApplicationContext ctx) {
+	public ProcessThread(stream.Process process, ApplicationContext ctx) {
 
-        Integer prio = Thread.NORM_PRIORITY;
-        try {
-            String prioValue = process.getProperties().get("priority");
-            if (prioValue == null)
-                prioValue = "normal";
+		Integer prio = Thread.NORM_PRIORITY;
+		try {
+			String prioValue = process.getProperties().get("priority");
+			if (prioValue == null)
+				prioValue = "normal";
 
-            if (PRIORITY_NAMES.containsKey(prioValue)) {
-                prioValue = PRIORITY_NAMES.get(prioValue).toString();
-            }
+			if (PRIORITY_NAMES.containsKey(prioValue)) {
+				prioValue = PRIORITY_NAMES.get(prioValue).toString();
+			}
 
-            prio = new Integer(prioValue);
-        } catch (Exception e) {
-            prio = Thread.NORM_PRIORITY;
-        }
+			prio = new Integer(prioValue);
+		} catch (Exception e) {
+			prio = Thread.NORM_PRIORITY;
+		}
 
-        if (prio > Thread.MAX_PRIORITY)
-            prio = Thread.MAX_PRIORITY;
+		if (prio > Thread.MAX_PRIORITY)
+			prio = Thread.MAX_PRIORITY;
 
-        if (prio < Thread.MIN_PRIORITY)
-            prio = Thread.MIN_PRIORITY;
+		if (prio < Thread.MIN_PRIORITY)
+			prio = Thread.MIN_PRIORITY;
 
-        this.setPriority(prio);
-        this.process = process;
-        this.context = ctx;
-    }
+		this.setPriority(prio);
+		this.process = process;
+		this.context = ctx;
+	}
 
-    public void addListener(ProcessListener l) {
-        processListener.add(l);
-    }
+	public void addListener(ProcessListener l) {
+		processListener.add(l);
+	}
 
-    public void removeListener(ProcessListener l) {
-        processListener.remove(l);
-    }
+	public void removeListener(ProcessListener l) {
+		processListener.remove(l);
+	}
 
-    public boolean isRunning() {
-        return running;
-    }
+	public boolean isRunning() {
+		return running;
+	}
 
-    public stream.Process getProcess() {
-        return process;
-    }
+	public stream.Process getProcess() {
+		return process;
+	}
 
-    public void init() throws Exception {
-        log.debug("Initializing process with process-context...");
-        process.init(context);
-    }
+	public void init() throws Exception {
+		log.debug("Initializing process with process-context...");
+		process.init(context);
+	}
 
-    public void run() {
-        running = true;
-        Exception error = null;
-        try {
+	public void run() {
+		running = true;
+		Exception error = null;
+		try {
 
-            log.debug("Starting process {}, notifying listeners {}", process, processListener);
-            for (ProcessListener l : this.processListener) {
-                log.debug("Calling process-listener {}", l);
-                l.processStarted(process);
-            }
-            executing.set(true);
-            process.execute();
-            executing.set(false);
-        } catch (InterruptedException ie) {
-            log.error("Process thread interruped while executing!?");
-            ie.printStackTrace();
-            executing.set(false);
-            error = ie;
-        } catch (Exception e) {
-            error = e;
-            StringWriter sw = new StringWriter();
-            e.printStackTrace(new PrintWriter(sw));
-            String exceptionDetails = sw.toString();
-            log.error(exceptionDetails);
-            // e.printStackTrace();
+			log.debug("Starting process {}, notifying listeners {}", process, processListener);
+			for (ProcessListener l : this.processListener) {
+				log.debug("Calling process-listener {}", l);
+				l.processStarted(process);
+			}
+			executing.set(true);
+			process.execute();
+			executing.set(false);
+		} catch (InterruptedException ie) {
+			log.error("Process thread interruped while executing!?");
+			ie.printStackTrace();
+			executing.set(false);
+			error = ie;
+		} catch (Exception e) {
+			error = e;
+			StringWriter sw = new StringWriter();
+			e.printStackTrace(new PrintWriter(sw));
+			String exceptionDetails = sw.toString();
+			log.error(exceptionDetails);
+			// e.printStackTrace();
 
-            for (ProcessListener l : this.processListener) {
-                log.debug("Calling process-listener {} for error handling", l);
-                l.processError(process, e);
-            }
+			for (ProcessListener l : this.processListener) {
+				log.debug("Calling process-listener {} for error handling", l);
+				l.processError(process, e);
+			}
 
-        } finally {
-            executing.set(false);
-            log.debug("ProcessThread shutting down...");
+		} finally {
+			executing.set(false);
+			log.debug("ProcessThread shutting down...");
 
-            try {
-                log.debug("Finishing processors...");
-                process.finish();
-            } catch (Exception e) {
-                log.error("Failed to finish: {}", e.getMessage());
-                e.printStackTrace();
-            }
+			try {
+				log.debug("Finishing processors...");
+				process.finish();
+			} catch (Exception e) {
+				log.error("Failed to finish: {}", e.getMessage());
+				e.printStackTrace();
+			}
 
-            log.debug("Process {} finished, notifying listeners: {}", process, processListener);
-            for (ProcessListener l : this.processListener) {
-                log.debug(" Calling listener {}", l);
-                l.processFinished(process);
-            }
+			log.debug("Process {} finished, notifying listeners: {}", process, processListener);
+			for (ProcessListener l : this.processListener) {
+				log.debug(" Calling listener {}", l);
+				l.processFinished(process);
+			}
 
-            running = false;
+			running = false;
 
-            if (error != null) {
-                throw new RuntimeException("Process stopped after exception: " + error.getMessage(), error.getCause());
-            }
-        }
-    }
+			if (error != null) {
+				error.printStackTrace();
+				throw new RuntimeException("Process stopped after exception: " + error.getMessage(), error.getCause());
+			}
+		}
+	}
 
-    /**
-     * @see streams.runtime.Hook#signal(int)
-     */
-    @Override
-    public void signal(int flags) {
-        log.debug("Signaling ProcessThread {}", this);
-        if (executing.get()) {
-            log.debug("   process thread is executing, sending interrupt() signal");
-            this.interrupt();
-        }
-    }
+	/**
+	 * @see streams.runtime.Hook#signal(int)
+	 */
+	@Override
+	public void signal(int flags) {
+		log.debug("Signaling ProcessThread {}", this);
+		if (executing.get()) {
+			log.debug("   process thread is executing, sending interrupt() signal");
+			this.interrupt();
+		}
+	}
 }
